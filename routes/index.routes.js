b68c991145ef86f0850284833dad7ae8fa7c3cd4
Evelina Neumann <evelina.neumann1@gmail.com>
2023-05-03 20:34:32 +0200

    working on home page pop up

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+// Handle GET requests to the home page after user confirms legal age
+router.get('/home', (req, res) => {
+  res.render('home', {});
+});
 
 module.exports = router;
