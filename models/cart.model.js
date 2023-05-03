49dfe018407f06ef9b5f82407001d4ffddd61cc8
Evelina Neumann <evelina.neumann1@gmail.com>
2023-05-03 19:18:02 +0200

    added cart model and index.js to combain cart&user

---
+++ models/cart.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const User = require("./User.model");
+
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cart = model("Cart", cartSchema);
+
+module.exports = Cart;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const User = require('.User.model');
+const Cart = require('./cart.model');
+
+module.exports = { User, Cart };
