51ee4c04a704becc66b9843b7067a437c7fcfe5f
Joaomco99 <joaomco99@gmail.com>
2023-05-05 22:58:03 +0100

    homepage.routes and homepage.hbs

--- routes/homepage.routes.js
+++ routes/homepage.routes.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
+const Wine = require('./models/Wine.model');
 
-// Define a route for the homepage
-router.get('/', (req, res) => {
-  // Connect to the MongoDB database
-  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send('Error connecting to database');
-    }
-    // Get a reference to the collection
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-    // Find all documents in the collection and return them as an array
-    collection.find({}).toArray((err, docs) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send('Error fetching data from database');
-      }
-      // Close the database connection
-      client.close();
-      // Render the homepage template with the retrieved data
-      res.render('homepage', { products: docs });
-    });
-  });
+router.get('/', async (req, res) => {
+  try {
+    const wines = await Wine.find({}, 'product_vibe product_name product_price');
+    res.render('homepage', { wines });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error retrieving wines');
+  }
 });
 
-
-
 module.exports = router;
+
+
