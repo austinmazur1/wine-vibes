537a3303cadd8e2d256895dc4231193c8fef436f
Austin Mazur <mazuraustin1@gmail.com>
2023-05-11 21:29:43 +0200

    Create cart routes to add products to the users cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -7,15 +7,77 @@ const User = require("../models/User.model");
 const Product = require("../models/Wine.model"); 
 
 
+//middle page route
+router.get('/add-item/:id', async(req, res, next) => {
+    res.render('cart/add-item')
+})
 
+//TODO create the cart post route
+//handles when user adds items to the cart
+router.post('/add-item/:id', async (req, res, next) => {
+try {
+    const userId = req.session.currentUser._id;
+
+    const id = req.body.id
+    console.log("product id", id);
+
+    const product = await Product.findById(id);
+    console.log("product", product);
+
+    let userCart = await Cart.findOne({ userId }).populate('items');
+
+    if (!userCart) {
+        userCart = new Cart({
+            userId,
+            items: [{
+                productId: product._id,
+                quantity: 1,
+                productName: product.product_name,
+                productPrice: product.product_price
+            }]
+        });
+    } else {
+        let item = userCart.items.find(item => item.productId.equals(product._id));
+        if (item) {
+            item.quantity += 1;
+            item.productName = product.product_name;
+            item.productPrice = product.product_price;
+        } else {
+            userCart.items.push({
+                productId: product._id,
+                quantity: 1,
+                productName: product.product_name,
+                productPrice: product.product_price
+            });
+        }
+    }
+
+    await userCart.save(); 
+    console.log("user cart", userCart);
+
+    //if cart doesnt exist, we create a new Cart
+
+    //will need to make sure when we populate, we dont get duplicates
+    //just add to the quantity
+    res.render("cart/add-item", {userCart,product, userInSession: req.session.currentUser});
+} catch (error) {
+    next(error);
+}
+})
+
+//Cart
 //Render the cart
-router.get('/:id', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
 
     //grab user id from session
     const id = req.session.currentUser._id;
 
     //with user id, find associated cart
-    const userCart = await Cart.findOne({user: id})
+    const userCart = await Cart.findOne({userId: id})
+   
+
+
+    console.log(userCart);
 
     //if no cart, render an "empty cart" page, link to homepage
     //else if, render their cart, send user and cart to access data in hbs
@@ -25,23 +87,6 @@ router.get('/:id', async (req, res, next) => {
         res.render('cart/cart', {userInSession: req.session.currentUser, userCart});    
     }
 });
-
-//TODO create the cart post route
-//handles when user adds items to the cart
-router.post('/:id', async (req, res, next) => {
-    console.log(req.session.currentUser._id);
-
-    //get user id, find the cart with it
-
-    //when user clicks add to cart, we get the id of that product
-
-    //with the id we can then populate the cart
-
-    //if cart doesnt exist, we create a new Cart
-
-    //will need to make sure when we populate, we dont get duplicates
-    //just add to the quantity
-})
 
 
 //Stripe checkout is in checkout.routes
