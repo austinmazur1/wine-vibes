ede6b41270ccc304464778f73611c1683864a71a
Austin Mazur <mazuraustin1@gmail.com>
2023-05-12 18:39:15 +0200

    Add function to checkout button, stripe is working for checkout

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -3,93 +3,89 @@ const router = express.Router();
 
 //Import the models
 const Cart = require("../models/cart.model");
-const User = require("../models/User.model");
-const Product = require("../models/Wine.model"); 
-
+const Product = require("../models/Wine.model");
 
 //middle page route
-router.get('/add-item/:id', async(req, res, next) => {
-    res.render('cart/add-item')
-})
+router.get("/add-item/:id", async (req, res, next) => {
+  res.render("cart/add-item");
+});
 
-//TODO create the cart post route
 //handles when user adds items to the cart
-router.post('/add-item/:id', async (req, res, next) => {
-try {
+router.post("/add-item/:id", async (req, res, next) => {
+  try {
     const userId = req.session.currentUser._id;
 
-    const id = req.body.id
+    const id = req.body.id;
     console.log("product id", id);
 
     const product = await Product.findById(id);
     console.log("product", product);
 
-    let userCart = await Cart.findOne({ userId }).populate('items');
+    let userCart = await Cart.findOne({ userId }).populate("items");
 
     if (!userCart) {
-        userCart = new Cart({
-            userId,
-            items: [{
-                productId: product._id,
-                quantity: 1,
-                productName: product.product_name,
-                productPrice: product.product_price
-            }]
-        });
+      userCart = new Cart({
+        userId,
+        items: [
+          {
+            productId: product._id,
+            quantity: 1,
+            productName: product.product_name,
+            productPrice: product.product_price,
+          },
+        ],
+      });
     } else {
-        let item = userCart.items.find(item => item.productId.equals(product._id));
-        if (item) {
-            item.quantity += 1;
-            item.productName = product.product_name;
-            item.productPrice = product.product_price;
-        } else {
-            userCart.items.push({
-                productId: product._id,
-                quantity: 1,
-                productName: product.product_name,
-                productPrice: product.product_price
-            });
-        }
+      let item = userCart.items.find((item) =>
+        item.productId.equals(product._id)
+      );
+      if (item) {
+        item.quantity += 1;
+        item.productName = product.product_name;
+        item.productPrice = product.product_price;
+      } else {
+        userCart.items.push({
+          productId: product._id,
+          quantity: 1,
+          productName: product.product_name,
+          productPrice: product.product_price,
+        });
+      }
     }
 
-    await userCart.save(); 
-    console.log("user cart", userCart);
+    await userCart.save();
 
-    //if cart doesnt exist, we create a new Cart
-
-    //will need to make sure when we populate, we dont get duplicates
-    //just add to the quantity
-    res.render("cart/add-item", {userCart,product, userInSession: req.session.currentUser});
-} catch (error) {
+    res.render("cart/add-item", {
+      userCart,
+      product,
+      userInSession: req.session.currentUser,
+    });
+  } catch (error) {
     next(error);
-}
-})
+  }
+});
 
 //Cart
 //Render the cart
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  //grab user id from session
+  const id = req.session.currentUser._id;
 
-    //grab user id from session
-    const id = req.session.currentUser._id;
+  //with user id, find associated cart
+  const userCart = await Cart.findOne({ userId: id });
 
-    //with user id, find associated cart
-    const userCart = await Cart.findOne({userId: id})
-   
+  console.log(userCart);
 
-
-    console.log(userCart);
-
-    //if no cart, render an "empty cart" page, link to homepage
-    //else if, render their cart, send user and cart to access data in hbs
-    if(!userCart) {
-        res.render('cart/empty-cart');
-    } else {
-        res.render('cart/cart', {userInSession: req.session.currentUser, userCart});    
-    }
+  //if no cart, render an "empty cart" page, link to homepage
+  //else if, render their cart, send user and cart to access data in hbs
+  if (!userCart) {
+    res.render("cart/empty-cart");
+  } else {
+    res.render("cart/cart", {
+      userInSession: req.session.currentUser,
+      userCart,
+    });
+  }
 });
-
-
-//Stripe checkout is in checkout.routes
-//dom manipulation is in public/js/script.js
 
 module.exports = router;
