54359937ee584b76ebbc96de732ab81851b2b89d
Evelina Neumann <evelina.neumann1@gmail.com>
2023-05-13 13:34:21 +0200

    added first styles

--- models/Wine.model.js
+++ models/Wine.model.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productDetailsSchema = new Schema({
-  product_vibe: { type: Object, required: true },
+  product_vibe: { type: String, required: true },
   product_name: { type: String, required: true },
-  product_image: { type: String, required: true},
-  product_price: { type: Number, required: true },
-  vintage: { type: String, required: true },
-  grape_variety: { type: String, required: true },
+  product_price: { type: String, required: true },
   bottle_size: { type: String, required: true },
   closure: { type: String, required: true },
   quality_level: { type: String, required: true },
